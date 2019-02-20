d2fb80feb9d56b10a2cb5400fb893369fb257a66
aboueich <47013540+aboueich@users.noreply.github.com>
2019-02-19 17:00:01 -0800

    Added planet classes and variables
    No functions (second sprint I'm assuming). I'm unsure of other variables each planet would need, so I just left it with the ones they have for now. Add them as you see fit.

---
+++ scripts/planet.js
@@ -0,0 +1,85 @@
+//planet.js
+
+//contains planet definitions. 
+
+class Planet
+{
+	constructor(x, y, cpx, cpy)
+	{
+		this.x = x;
+		this.y = y;
+		this.cpx = cpx;
+		this.cpy = cpy;
+		
+		//Can be changed in the future
+		this.radius = 2;
+		this.minLength = cpx - 1;
+		this.maxLength = cpx + 1;
+		this.minHeight = cpy - 1;
+		this.maxHeight = cpy + 1;	
+
+		this.sprite = new Image();
+		//x is the number of each planet (1 - 7)
+		//this.sprite.src = "img/planet[x].png";
+	}		
+}
+
+class Celeron {
+	constructor(x, y, cpx, cpy)
+	{
+		this.x = x;
+		this.y = y;
+		this.cpx = cpx;
+		this.cpy = cpy;
+		
+		//Can be changed in the future
+		this.radius = 2;
+		this.minLength = cpx - 1;
+		this.maxLength = cpx + 1;
+		this.minHeight = cpy - 1;
+		this.maxHeight = cpy + 1;	
+
+		this.sprite = new Image();
+		//this.sprite.src = "img/celeron.png";
+	}
+}
+
+class Xeon{ 
+	constructor(x, y, cpx, cpy)
+	{
+		this.x = x;
+		this.y = y;
+		this.cpx = cpx;
+		this.cpy = cpy;
+		
+		//Can be changed in the future. For now, all planets have a radius of 2.
+		this.radius = 2;
+		this.minLength = cpx - 1;
+		this.maxLength = cpx + 1;
+		this.minHeight = cpy - 1;
+		this.maxHeight = cpy + 1;	
+
+		this.sprite = new Image();
+		//this.sprite.src = "img/xeon.png";
+	}
+}
+
+class Ryzen { 
+	constructor(x, y, cpx, cpy)
+	{
+		this.x = x;
+		this.y = y;
+		this.cpx = cpx;
+		this.cpy = cpy;
+		
+		//Can be changed in the future. For now, all planets have a radius of 2.
+		this.radius = 2;
+		this.minLength = cpx - 1;
+		this.maxLength = cpx + 1;
+		this.minHeight = cpy - 1;
+		this.maxHeight = cpy + 1;	
+
+		this.sprite = new Image();
+		//this.sprite.src = "img/ryzen.png";
+	}
+}
