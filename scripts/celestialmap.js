282854d4bd866935e77fde0d3cda6c40ee00c6e5
Brendan Tschuy <brendantschuy@gmail.com>
2019-02-20 21:52:42 -0800

    Added comments contextualizing last change

--- scripts/celestialmap.js
+++ scripts/celestialmap.js
@@ -3,86 +3,52 @@ var mapScale = 32;  //ratio of px on game to px on map
 
 function showMap(obstacles){
   var test = document.getElementById("mapCanvas");
-  if(!test){
-    var themap = document.createElement("canvas");
-    themap.id = "mapCanvas";
-    var map = themap.getContext('2d');
 
-    //WIP: Trying to get map to work better at lower screen resolution
+  //Use "test" if mapCanvas is NOT dynamically created
+  if(!test){
+    var mapCvs = document.createElement("canvas");
+    mapCvs.id = "mapCanvas";
+    var mapCtx = mapCvs.getContext('2d');
+
+    //Makes map smaller below a certain screen resolution threshold
     if(window.screen.availWidth < 1367)
     {
       mapRatio = 1.5;
     }
 
-    //map.fillStyle = "red";
-    //map.fillRect(0, 0, 300, 300);
-    themap.height = (MAP_HEIGHT * GRID_SIZE)/mapRatio/1.1; //581 px or 387 px, depending on screen res
-    themap.width = (MAP_WIDTH * GRID_SIZE)/mapRatio/1.1;   //581 px or 387 px, depending on screen res
+    mapCvs.height = (MAP_HEIGHT * GRID_SIZE)/mapRatio/1.1; //581 px or 387 px, depending on screen res
+    mapCvs.width = (MAP_WIDTH * GRID_SIZE)/mapRatio/1.1;   //581 px or 387 px, depending on screen res
 
-    map.fillStyle = "RGBA(100, 100, 100, 0.3)";
-    map.fillRect(0, 0, themap.height, themap.width);
+    //Creates background for testing purposes, will be replaced w/ scroll
+    mapCtx.fillStyle = "RGBA(100, 100, 100, 0.3)";
+    mapCtx.fillRect(0, 0, mapCvs.height, mapCvs.width);
 
-
-    //themap.style.left = 29.5;
-    //themap.style.top = -3.5;
-    //document.getElementById("map").style.backgroundSize = themap.height + "px " +  themap.width + "px";
+    //Commented out text is for scroll background pic:
+    //mapCvs.style.left = 29.5;
+    //mapCvs.style.top = -3.5;
+    //document.getElementById("map").style.backgroundSize = mapCvs.height + "px " +  mapCvs.width + "px";
     //var backgroundImage = new Image();
     //backgroundImage.src = "img/parchment.jpg";
     //map.drawImage(backgroundImage, 0, 0);
 
-    //context.fillRect(0, 0, canvas.width, canvas.height);
-    
-    //themap.style.position = "relative";
-    //themap.style.left = (MAP_WIDTH * GRID_SIZE)/3.5;
-    //themap.style.top = (MAP_HEIGHT * GRID_SIZE)/2.5;
-    //map.beginPath();
-    //map.save();
-    //map.translate(SHIP_ABS_X, SHIP_ABS_Y); 
+    //Determines where to put map canvas. Depends on screen resolution.
     var largeMap = document.getElementById("widescreenOnly");
     if(window.getComputedStyle(largeMap).display === "none")
     {
-      document.getElementById("dev-buttons").appendChild(themap);
+      document.getElementById("dev-buttons").appendChild(mapCvs);
     }
     else
     {
-      document.getElementById("dev").appendChild(themap);
+      document.getElementById("dev").appendChild(mapCvs);
     }
-    //document.getElementById("dev").appendChild(themap);
-    //drawGridMap("theMap");
 
     drawThingsMap("mapCanvas",obstacles);   
 
   }else {
-    //switchVisibility("theMap");
+    //switchVisibility("mapCvs");
     drawThingsMap("mapCanvas",obstacles);
   }
 }
-
-//Never actually called:
-/*function drawGridMap(elementID) {
-    var ctx = document.getElementById(elementID).getContext('2d');
-
-    //creates backdrop (opacity = 0.4 so it is see-through)
-      ctx.fillStyle = "RGBA(0, 0, 0, 0.4)";
-      ctx.fillRect(0, 0, GAME_SCREEN_WIDTH, GAME_SCREEN_HEIGHT);
-
-      ctx.beginPath();    //reduces lag       
-      ctx.strokeStyle = "white";
-      for(w = 0; w < GAME_SCREEN_WIDTH; w += GRID_SIZE/2)
-      {
-          for(h = 0; h < GAME_SCREEN_HEIGHT; h += GRID_SIZE/2)
-          {
-              //draws line every 128 px in either direction
-              ctx.moveTo(w, 0.5);
-              ctx.lineTo(w, GAME_SCREEN_WIDTH);
-              ctx.stroke();
-              ctx.moveTo(0.5, h);
-              ctx.lineTo(GAME_SCREEN_HEIGHT, h);
-              ctx.stroke();
-          }
-      }
-      ctx.closePath();
-  }*/
 
 function drawThingsMap(elementID,obstacles) {
   var ctx = document.getElementById(elementID).getContext('2d');
@@ -95,7 +61,7 @@ function drawThingsMap(elementID,obstacles) {
 
 //draws all obstacles (for now, just asteroids)
 function drawObstaclesMap(ctx,obstacles) {
-  //themap = document.getElementById("map");
+  //mapCvs = document.getElementById("map");
   obstacles.forEach(function (item)
   {
     if(item.visible){
@@ -108,6 +74,7 @@ function drawObstaclesMap(ctx,obstacles) {
           ctx.fillStyle = "red";
           break;
         case("Celeron") : case("Xeon") : case ("Ryzen") : 
+          //Write names on map too, to make sure we fulfill user story
           ctx.fillStyle = "blue";
           ctx.fillText(objName, (item.x/mapScale)/mapRatio + 5, (item.y/mapScale)/mapRatio);
           break;
@@ -122,8 +89,7 @@ function drawObstaclesMap(ctx,obstacles) {
           break;
       }
 
-      //alert(rock.x/mapScale/mapRatio + " and " + rock.y/mapScale/mapRatio);
-      ctx.fillRect((item.x/mapScale)/mapRatio,(item.y/mapScale)/mapRatio,5,5);
+      ctx.fillRect((item.x/mapScale)/mapRatio, (item.y/mapScale)/mapRatio, 5, 5);
 
       ctx.closePath();
     }
