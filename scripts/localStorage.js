cb7f5382f1f9b30616b81d6baab9727d04a8e6de
Bryttanie <bhouse@pdx.edu>
2019-03-06 17:04:53 -0800

    allow user input

--- scripts/localStorage.js
+++ scripts/localStorage.js
@@ -14,9 +14,18 @@ var gameState = {
   XeonX: 0, //Planet.Xeon.cpx,     
   XeonY: 0, //Planet.Xeon.cpy,    
   RyzenX: 0, //Planet.Ryzen.cpx,   
-  RyzenY: 0, //Planet.Ryzen.cpy,    
+  RyzenY: 0, //Planet.Ryzen.cpy,   
+  DeathStarX: 0, // DeathStar.cpx 
+  DeathStarY: 0, // DeathStar.cpy
   RecipeX: 0, //Recipe.cpx,   
   RecipeY: 0, // Recipe.cpy,  
+  BadMaxX: 0, // BadMax.cpx,
+  BadMaxY: 0, // BadMax.cpy,
+  AbandonedFreighterX: 0, // AbandonedFreighter.cpx
+  AbandonedFreighterY: 0, // AbandonedFreighter.cpy
+  SpaceStationX: 0, // SpaceStation.cpx
+  SpaceStationY: 0, // SpaceStation.cpy
+
   activeGame: false
 };
 
@@ -41,30 +50,32 @@ function displaySaved(savedList) {
 }
 
 // saves game state to browser 
-function save(gameState, savedList) {
+function save() {
   if (!supportsLocalStorage()) {
     console.log("Browser does not support localStorage!");
     return false; 
   }
-  localStorage.setItem("state", JSON.stringify(gameState)); // change to allow user input for state name
+  var savedState = prompt("Enter a name for this game.")
+  localStorage.setItem(savedState, JSON.stringify(gameState)); 
   activeGame = true;
   savedList.push(gameState);  
 }
 
 // loads game from browser 
-function load(gameState, savedList) {
-  var response;
+function load(savedState) {
+  //var response;
 
   if (!supportsLocalStorage() || localStorage["activeGame"] == "false") {
     console.log("Browser does not support localStorage!");
     return false; 
   }  
-  displaySaved(); // display list of saved games
+ /* displaySaved(); // display list of saved games
   response = prompt("Which game would you like to load? Enter a number, starting at 1.");
   for(var i = 0; i < savedList.length; i++) {
     if(response == (i+1)) 
       gameState = JSON.parse(localStorage.getItem(savedList[i+1]));
-  }
+  }*/
+  gameState = JSON.parse(localStorage.getItem(savedState));
 }
 
 // clears entire storage (might not need as separate function)
